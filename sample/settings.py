fa1741bf02c98844e7ca852d2dd465026f0dc276
upender-sg <upendershanagonda79@gmail.com>
2022-03-10 16:01:24 +0100

    debug

--- sample/settings.py
+++ sample/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1&v&jkk(f1=tf#itz)i&8#2xf8rdl%bokxzgdk9hff@efy9v9c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
